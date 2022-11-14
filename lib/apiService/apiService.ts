d22cf2df3bc3bd726db74eaed8b58ed5ba15c284
Ole Walberg <greblavelo@gmail.com>
2022-11-14 15:57:41 +0100

    i still have no idea what im doing

--- lib/apiService/apiService.ts
+++ lib/apiService/apiService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
+	baseURL: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
 });
 
 export default api;
